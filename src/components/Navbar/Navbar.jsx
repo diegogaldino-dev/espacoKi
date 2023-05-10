d7cac69950096b723491d97442fd48d53ac928ed
Diego Galdino <diegogaldino-dev@gmail.com>
2023-05-10 11:27:05 -0300

    ajustando link

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,23 +5,24 @@ import NavbarBootstrap from 'react-bootstrap/Navbar';
 import NavbarToggle from 'react-bootstrap/NavbarToggle';
 import NavbarCollapse from 'react-bootstrap/NavbarCollapse';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Logo from '../../img/logo/img9.svg'
+import { Link } from 'react-router-dom';
+import Logo from '../../img/logo/img9.svg';
 
-import { TextLogo, ContainerLogo, CustomNav } from './style'
+import { TextLogo, ContainerLogo, CustomNav } from './style';
 
 function CustomNavbar() {
   return (
-    <NavbarBootstrap expand="lg" style={{backgroundColor: '#608263'}} variant="dark">
+    <NavbarBootstrap expand="lg" style={{ backgroundColor: '#608263' }} variant="dark">
       <Container>
         <ContainerLogo>
-          <NavbarBootstrap.Brand href="/">
+          <NavbarBootstrap.Brand as={Link} to="/">
             <img
               alt="Logo da Ki Terapia"
               src={Logo}
-              margin-top="-20px"
-              margin-bottom="-20px"
+              style={{ marginTop: '-20px', marginBottom: '-20px' }}
               width="90"
-              height="90" className="d-inline-block align-top"
+              height="90"
+              className="d-inline-block align-top"
             />{' '}
             <TextLogo>
               Espaço Ki
@@ -31,17 +32,17 @@ function CustomNavbar() {
         <NavbarToggle aria-controls="navbar-nav" />
         <NavbarCollapse id="navbar-nav">
           <CustomNav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="sobre">Sobre</Nav.Link>
-            <Nav.Link href="servicos">Serviços</Nav.Link>
-            <Nav.Link href="contato">Contato</Nav.Link>
-            <Nav.Link href="artigos">Artigos</Nav.Link>
-            <Nav.Link href="catalogo">Catalogo</Nav.Link>
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/sobre">Sobre</Nav.Link>
+            <Nav.Link as={Link} to="/servicos">Serviços</Nav.Link>
+            <Nav.Link as={Link} to="/contato">Contato</Nav.Link>
+            <Nav.Link as={Link} to="/artigos">Artigos</Nav.Link>
+            <Nav.Link as={Link} to="/catalogo">Catalogo</Nav.Link>
           </CustomNav>
         </NavbarCollapse>
       </Container>
     </NavbarBootstrap>
-  )
+  );
 }
 
 export default CustomNavbar;
