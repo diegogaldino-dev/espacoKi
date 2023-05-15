7d624d7a80f09a32e5049bcb46d55558539fefb3
Diego Galdino <diegogaldino-dev@gmail.com>
2023-05-14 22:01:19 -0300

    ajudando banco de dados

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDWcD0PlJfMN9Mu-5rGsKBTOoQ7n-P6BJQ",
-  authDomain: "bd-adicionarartigo.firebaseapp.com",
-  projectId: "bd-adicionarartigo",
-  storageBucket: "bd-adicionarartigo.appspot.com",
-  messagingSenderId: "640917001232",
-  appId: "1:640917001232:web:c9d00e146a96228dbda695"
+  apiKey: "AIzaSyCsUY8P7jMwieN3OHhcV5D12sw6GiEJFMY",
+  authDomain: "bd-adicionarartigo-3ec38.firebaseapp.com",
+  databaseURL: "https://bd-adicionarartigo-3ec38-default-rtdb.firebaseio.com",
+  projectId: "bd-adicionarartigo-3ec38",
+  storageBucket: "bd-adicionarartigo-3ec38.appspot.com",
+  messagingSenderId: "325418594225",
+  appId: "1:325418594225:web:5af545843e1b39b2455790"
 };
 
 const app = initializeApp(firebaseConfig);
--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,12 +1,11 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyANql8tek25wC5P7aUO5d1g5aA2y1ys26M",
-  authDomain: "adicionarartigo.firebaseapp.com",
-  databaseURL: "https://adicionarartigo-default-rtdb.firebaseio.com",
-  projectId: "adicionarartigo",
-  storageBucket: "adicionarartigo.appspot.com",
-  messagingSenderId: "743829430758",
-  appId: "1:743829430758:web:0f3d89906637df7642b627",
-  measurementId: "G-9RKGPSMH3Z"
+  apiKey: "AIzaSyCsUY8P7jMwieN3OHhcV5D12sw6GiEJFMY",
+  authDomain: "bd-adicionarartigo-3ec38.firebaseapp.com",
+  databaseURL: "https://bd-adicionarartigo-3ec38-default-rtdb.firebaseio.com",
+  projectId: "bd-adicionarartigo-3ec38",
+  storageBucket: "bd-adicionarartigo-3ec38.appspot.com",
+  messagingSenderId: "325418594225",
+  appId: "1:325418594225:web:5af545843e1b39b2455790"
 };
 
 export default firebaseConfig;
